13dbb4454f83f5fe5093a66893d925024df1a978
KAST13L <o4enbsmeshno@gmail.com>
2022-11-01 21:22:07 +0200

    Updates

---
+++ precache-manifest.1088f8aa5619fd7a64518bf5ecc764c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a02b479b8e695b7d1646abf4bdbb70",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3470f570fbb9a9e54d08",
+    "url": "/todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "b044572710002cf7279a",
+    "url": "/todolist/static/js/2.9e3f98e4.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.9e3f98e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3470f570fbb9a9e54d08",
+    "url": "/todolist/static/js/main.695191e9.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
