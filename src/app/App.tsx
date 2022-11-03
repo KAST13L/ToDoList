5c5c666a10568f0f35c7711bbacbf75844988729
KAST13L <o4enbsmeshno@gmail.com>
2022-11-03 16:45:08 +0200

    fix: change scss to appBar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,15 @@
 import React, {useCallback, useEffect} from 'react'
 import './App.css'
 import {AppBar, Button, CircularProgress, Container, LinearProgress, Toolbar, Typography} from '@material-ui/core'
-import {TodolistsList} from '../features/TodolistsList'
+import {todolistsActions, TodolistsList} from '../features/TodolistsList'
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
 import {useSelector} from 'react-redux'
 import {appActions} from '../features/Application'
 import {Route} from 'react-router-dom'
 import {authActions, authSelectors, Login} from '../features/Auth'
 import {selectIsInitialized, selectStatus} from '../features/Application/selectors'
-import {useActions} from '../utils/redux-utils'
+import {useActions, useAppDispatch} from '../utils/redux-utils'
+import {AddItemForm, AddItemFormSubmitHelperType} from "../components/AddItemForm/AddItemForm";
 
 type PropsType = {
     demo?: boolean
@@ -19,6 +20,7 @@ function App({demo = false}: PropsType) {
     const isInitialized = useSelector(selectIsInitialized)
     const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn)
 
+    const dispatch = useAppDispatch()
     const {logout} = useActions(authActions)
     const {initializeApp} = useActions(appActions)
 
@@ -32,6 +34,24 @@ function App({demo = false}: PropsType) {
         logout()
     }, [])
 
+
+    const addTodolistCallback = useCallback(async (title: string, helper: AddItemFormSubmitHelperType) => {
+        let thunk = todolistsActions.addTodolistTC(title)
+        const resultAction = await dispatch(thunk)
+
+        if (todolistsActions.addTodolistTC.rejected.match(resultAction)) {
+            if (resultAction.payload?.errors?.length) {
+                const errorMessage = resultAction.payload?.errors[0]
+                helper.setError(errorMessage)
+            } else {
+                helper.setError('Some error occurred')
+            }
+        } else {
+            helper.setTitle('')
+        }
+    }, [])
+
+
     if (!isInitialized) {
         return <div
             style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
@@ -42,16 +62,19 @@ function App({demo = false}: PropsType) {
     return (
         <div className="App">
             <ErrorSnackbar/>
-            <AppBar position="static">
-                <Toolbar style={{display:'flex', justifyContent:'space-between'}}>
-                    <Typography variant="h3">
-                        TODO LIST
-                    </Typography>
-                    {isLoggedIn && <Button color="inherit" onClick={logoutHandler}>Log out</Button>}
+            <AppBar position="static" color='transparent'>
+                <Toolbar style={{display: 'flex', justifyContent: 'space-between', }}>
+                    <div style={{display:'flex', flexDirection:'row', width: '750px', justifyContent:'space-between', padding: '10px'}}>
+                        <Typography variant="h3">
+                            TODO LIST
+                        </Typography>
+                        {isLoggedIn && <AddItemForm addItem={addTodolistCallback}/>}
+                    </div>
+                    {isLoggedIn && <Button color="secondary" onClick={logoutHandler}>Log out</Button>}
                 </Toolbar>
-                <div style={{height:'1px'}}>{status === 'loading' && <LinearProgress/>}</div>
+                <div style={{height: '1px'}}>{status === 'loading' && <LinearProgress/>}</div>
             </AppBar>
-            <Container fixed>
+            <Container fixed style={{paddingTop: '25px'}}>
                 <Route exact path={'/'} render={() => <TodolistsList demo={demo}/>}/>
                 <Route path={'/login'} render={() => <Login/>}/>
             </Container>
--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -1,15 +1,14 @@
-import React, {useCallback, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import {useSelector} from 'react-redux'
 import {TodolistDomainType} from './todolists-reducer'
 import {TasksStateType} from './tasks-reducer'
 import {Grid} from '@material-ui/core'
-import {AddItemForm, AddItemFormSubmitHelperType} from '../../components/AddItemForm/AddItemForm'
 import {Todolist} from './Todolist/Todolist'
 import {Redirect} from 'react-router-dom'
 import {selectIsLoggedIn} from '../Auth/selectors'
 import {todolistsActions} from './index'
 import {AppRootStateType} from '../../utils/types'
-import {useActions, useAppDispatch} from '../../utils/redux-utils'
+import {useActions} from '../../utils/redux-utils'
 
 type PropsType = {
     demo?: boolean
@@ -20,26 +19,7 @@ export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
     const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
     const isLoggedIn = useSelector(selectIsLoggedIn)
 
-    const dispatch = useAppDispatch()
-
     const {fetchTodolistsTC} = useActions(todolistsActions)
-
-    const addTodolistCallback = useCallback(async (title: string, helper: AddItemFormSubmitHelperType) => {
-        let thunk = todolistsActions.addTodolistTC(title)
-        const resultAction = await dispatch(thunk)
-
-        if (todolistsActions.addTodolistTC.rejected.match(resultAction)) {
-            if (resultAction.payload?.errors?.length) {
-                const errorMessage = resultAction.payload?.errors[0]
-                helper.setError(errorMessage)
-            } else {
-                helper.setError('Some error occurred')
-            }
-        } else {
-            helper.setTitle('')
-        }
-    }, [])
-
 
     useEffect(() => {
         if (demo || !isLoggedIn) {
@@ -48,15 +28,11 @@ export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
         fetchTodolistsTC()
     }, [])
 
-
     if (!isLoggedIn) {
         return <Redirect to={'/login'}/>
     }
 
     return <>
-        <Grid container style={{padding: '20px'}}>
-            <AddItemForm addItem={addTodolistCallback}/>
-        </Grid>
         <Grid container spacing={10}>
             {
                 todolists.map(tl => {
