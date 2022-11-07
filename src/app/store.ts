1fdadb92d8de58636afe15efb31a2b647545bb6c
KAST13L <o4enbsmeshno@gmail.com>
2022-11-07 13:33:51 +0200

    feat: creating a hook that simplifies use useSelector

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,22 +4,25 @@ import {appReducer} from '../features/Application'
 import {authReducer} from '../features/Auth'
 import {tasksReducer, todolistsReducer} from '../features/TodolistsList'
 import {configureStore} from '@reduxjs/toolkit'
+import {TypedUseSelectorHook, useSelector} from "react-redux";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
+// обєднуємо редюсери за допомогою combineReducers,
 export const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer,
     todolists: todolistsReducer,
     tasks: tasksReducer
 })
-// непосредственно создаём store
+// створюємо store
 //export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
 })
+
+export type RootStateType = ReturnType<typeof store.getState>
+export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
