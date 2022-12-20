7558c3a74b1d8fb6f87492e2c2b5877fb1388417
KAST13L <o4enbsmeshno@gmail.com>
2022-12-20 11:15:45 +0200

    fix: kill UI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,28 +5,21 @@ import {useDispatch, useSelector} from 'react-redux'
 import {AppRootStateType} from './store'
 import {initializeAppWSAC, RequestStatusType} from './app-reducer'
 import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
-import {Menu} from '@mui/icons-material';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
 import {Navigate, Route, Routes} from "react-router-dom";
 import {Login} from "../features/TodolistsList/Login/Login";
 import {CircularProgress} from "@mui/material";
 import {logoutWorkerSagaAC} from "../features/TodolistsList/Login/auth-reducer";
 
-type PropsType = {
-    demo?: boolean
-}
 
 const selectStatus = (state: AppRootStateType): RequestStatusType => state.app.status
 const selectIsInitialized = (state: AppRootStateType): boolean => state.app.isInitialized
 const selectIsLoggedIn = (state: AppRootStateType): boolean => state.auth.isLoggedIn
 
-function App({demo = false}: PropsType) {
+export const App = () => {
     const status = useSelector(selectStatus)
     const isInitialized = useSelector(selectIsInitialized)
     const isLoggedIn = useSelector(selectIsLoggedIn)
@@ -36,8 +29,6 @@ function App({demo = false}: PropsType) {
         dispatch(initializeAppWSAC())
     }, [])
 
-    if (demo) {
-    }
     if (!isInitialized) {
         return <CircularProgress
             style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}/>
@@ -46,31 +37,18 @@ function App({demo = false}: PropsType) {
     return (
         <div className="App">
             <ErrorSnackbar/>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    {isLoggedIn && <Button onClick={() => {
-                        dispatch(logoutWorkerSagaAC())
-                    }} color="inherit">Log out </Button>}
-                </Toolbar>
+            <div>
+                {isLoggedIn && <Button onClick={() => {
+                    dispatch(logoutWorkerSagaAC())
+                }} color="inherit">Log out </Button>}
                 {status === 'loading' && <LinearProgress/>}
-            </AppBar>
-            <Container fixed>
-                <Routes>
-                    <Route path='/' element={<TodolistsList/>}/>
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
-                    <Route path='*' element={<Navigate to={'/404'}/>}/>
-                </Routes>
-            </Container>
-
+            </div>
+            <Routes>
+                <Route path='/' element={<TodolistsList/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
+                <Route path='*' element={<Navigate to={'/404'}/>}/>
+            </Routes>
         </div>
     )
 }
-
-export default App
--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -17,8 +17,6 @@ import {
     updateTaskWorkerSagaAC
 } from './tasks-reducer'
 import {TaskStatuses} from '../../api/todolists-api'
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import {AddItemForm} from '../../components/AddItemForm/AddItemForm'
 import {Todolist} from './Todolist/Todolist'
 import {Navigate} from "react-router-dom";
@@ -57,8 +55,7 @@ export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
     }, [])
 
     const changeFilter = useCallback(function (value: FilterValuesType, todolistId: string) {
-        const action = changeTodolistFilterAC(todolistId, value)
-        dispatch(action)
+        dispatch(changeTodolistFilterAC(todolistId, value))
     }, [])
 
     const removeTodolist = useCallback(function (id: string) {
@@ -79,32 +76,25 @@ export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
     }
 
     return <>
-        <Grid container style={{padding: '20px'}}>
-            <AddItemForm addItem={addTodolist}/>
-        </Grid>
-        <Grid container spacing={3}>
-            {
-                todolists.map(tl => {
-                    let allTodolistTasks = tasks[tl.id]
+        <AddItemForm addItem={addTodolist}/>
+        {
+            todolists.map(tl => {
+                let allTodolistTasks = tasks[tl.id]
 
-                    return <Grid item key={tl.id}>
-                        <Paper style={{padding: '10px'}}>
-                            <Todolist
-                                todolist={tl}
-                                tasks={allTodolistTasks}
-                                removeTask={removeTask}
-                                changeFilter={changeFilter}
-                                addTask={addTask}
-                                changeTaskStatus={changeStatus}
-                                removeTodolist={removeTodolist}
-                                changeTaskTitle={changeTaskTitle}
-                                changeTodolistTitle={changeTodolistTitle}
-                                demo={demo}
-                            />
-                        </Paper>
-                    </Grid>
-                })
-            }
-        </Grid>
+                return <Todolist
+                    key={tl.id}
+                    todolist={tl}
+                    tasks={allTodolistTasks}
+                    removeTask={removeTask}
+                    changeFilter={changeFilter}
+                    addTask={addTask}
+                    changeTaskStatus={changeStatus}
+                    removeTodolist={removeTodolist}
+                    changeTaskTitle={changeTaskTitle}
+                    changeTodolistTitle={changeTodolistTitle}
+                    demo={demo}
+                />
+            })
+        }
     </>
 }
