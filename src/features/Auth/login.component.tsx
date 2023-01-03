35a9e34f8c035e8bebe51c030de9c4f77c8364df
KAST13L <o4enbsmeshno@gmail.com>
2023-01-03 23:23:03 +0200

    feat: refactor before merge branch

--- src/features/Auth/login.component.tsx
+++ src/features/Auth/login.component.tsx
@@ -8,6 +8,7 @@ import Button from "@mui/material/Button";
 import {Navigate} from "react-router-dom";
 import {loginWorkerSagaAC} from "@app/features/Auth/auth-reducer";
 import {useFormik} from "formik";
+import React from "react";
 
 
 type FormValuesType = {
@@ -49,47 +50,50 @@ export const Login = () => {
         return <Navigate to={'/'}/>
     }
 
-    return <span className='flex justify-center'>
-        <Paper elevation={6}
-               className=' flex justify-center items-center w-[400px] h-[500px]'><form
-            onSubmit={formik.handleSubmit}>
-            <FormControl>
-                <span className='flex justify-center '>
-                    <h2 className='font-serif text-4xl mb-4'>Login</h2>
-                </span>
-                <FormGroup>
-                    <TextField
-                        label="Email"
-                        margin="normal"
-                        {...formik.getFieldProps("email")}
-                    />
-                    <TextField
-                        type="password"
-                        label="Password"
-                        margin="normal"
-                        {...formik.getFieldProps("password")}
-                    />
-                    <FormControlLabel
-                        label={'Remember me'}
-                        control={<Checkbox
-                            {...formik.getFieldProps("rememberMe")}
-                            checked={formik.values.rememberMe}
-                        />}
-                    />
-                    <Button type={'submit'} variant={'contained'} color={'primary'}
-                            style={{marginBottom: '10px'}}>Login</Button>
-                </FormGroup>
-                <FormLabel>
-                    <span className='text-center m-2 '>
-                    <p>To log in get registered <a
-                        href={'https://social-network.samuraijs.com/'}
-                        target={'_blank'}>here</a></p>
-                        <p>or use common test account credentials:</p><hr className='m-2'/>
-                        <p>Email: smykdav@gmail.com</p>
-                        <p>Password: asdasdasd</p>
-                    </span>
-
-                </FormLabel></FormControl></form></Paper>
-    </span>
+    return (
+        <span className='flex justify-center'>
+            <Paper elevation={6}
+                   className=' flex justify-center items-center w-[400px] h-[500px]'>
+                <form onSubmit={formik.handleSubmit}>
+                    <FormControl>
+                        <span className='flex justify-center '>
+                            <h2 className='font-serif text-4xl mb-4'>Login</h2>
+                        </span>
+                    <FormGroup>
+                            <TextField
+                                label="Email"
+                                margin="normal"
+                                {...formik.getFieldProps("email")}/>
+                            <TextField
+                                type="password"
+                                label="Password"
+                                margin="normal"
+                                {...formik.getFieldProps("password")}
+                            />
+                            <FormControlLabel
+                                label={'Remember me'}
+                                control={<Checkbox
+                                    {...formik.getFieldProps("rememberMe")}
+                                    checked={formik.values.rememberMe}
+                                />}
+                            />
+                            <Button type={'submit'} variant={'contained'}
+                                    color={'primary'}
+                                    style={{marginBottom: '10px'}}>Login</Button>
+                    </FormGroup>
+                    <FormLabel>
+                        <span className='text-center m-2 '>
+                        <p>To log in get registered <a
+                            href={'https://social-network.samuraijs.com/'}
+                            target={'_blank'}>here</a></p>
+                            <p>or use common test account credentials:</p><hr
+                            className='m-2'/>
+                            <p>Email: smykdav@gmail.com</p>
+                            <p>Password: asdasdasd</p>
+                        </span>
+                    </FormLabel>
+                </FormControl>
+            </form>
+        </Paper>
+    </span>)
 }
-
