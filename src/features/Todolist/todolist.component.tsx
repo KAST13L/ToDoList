e8e3b9d13ef35b87ef9c4429093c1e3c195f3ddc
KAST13L <o4enbsmeshno@gmail.com>
2023-01-03 21:17:23 +0200

    fix: edit todolist title css

--- src/features/Todolist/todolist.component.tsx
+++ src/features/Todolist/todolist.component.tsx
@@ -61,19 +61,17 @@ export const Todolist: FC<PropsType> = React.memo(function ({demo = false, ...pr
         tasksForTodolist = props.tasks.filter(t => t.status === TaskStatuses.Completed)
     }
 
-    return <Paper elevation={8} className='flex-row w-[290px] m-4 p-4'>
-        <div className='min-h-[30px] h-auto'>
-            <div className='relative h-[30px] text-2xl font-extrabold'>
-                <EditableSpan value={props.todolist.title}
-                              onChange={changeTodolistTitle}/>
-                <span className='absolute left-[235px] bottom-[6px]'>
-                <IconButton onClick={removeTodolist}
-                            disabled={props.todolist.entityStatus === 'loading'}>
-                    <Delete/>
-                </IconButton>
-            </span>
+    return <Paper elevation={8} className='w-[290px] m-4 p-4'>
+            <div className='relative w-[250px] text-2xl font-extrabold'>
+                    <EditableSpan value={props.todolist.title}
+                                  onChange={changeTodolistTitle}/>
+                <span className='absolute left-[235px] top-[-17px]'>
+                    <IconButton onClick={removeTodolist}
+                                disabled={props.todolist.entityStatus === 'loading'}>
+                        <Delete/>
+                    </IconButton>
+                </span>
             </div>
-        </div>
         <div className='ml-[5px] my-2'>
             <AddItemForm addItem={addTask}
                          disabled={props.todolist.entityStatus === 'loading'}/>
