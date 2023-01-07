516b3d046e255d73789af0e46505fcd746894217
KAST13L <o4enbsmeshno@gmail.com>
2023-01-07 11:11:21 +0200

    feat: added stories for App.tsx

---
+++ src/app/App.stories.tsx
@@ -0,0 +1,12 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import React from 'react';
+import {App} from './App';
+
+export default {
+    title: 'App/App',
+    component: App,
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = (args) => <App {...args} />;
+
+export const View = Template.bind({});
