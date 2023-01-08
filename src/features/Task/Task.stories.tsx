e72115b5fbe41a50e433bb167ff8806e6b192a08
KAST13L <o4enbsmeshno@gmail.com>
2023-01-08 12:17:53 +0200

    fix: fix AddItemForm.stories.tsx

---
+++ src/features/Task/Task.stories.tsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react'
+import {action} from '@storybook/addon-actions'
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {Task} from "@app/features/Task/task.component";
+import {TaskPriorities, TaskStatuses, TaskType} from "@app/api/todolists-api";
+
+export default {
+    title: 'Element TODO/Task',
+    component: Task,
+    decorators: [
+        (StoryFn) => {
+            const [title, setTitle] = useState<string>('Task Task Task');
+
+            const [task, setTask] = useState<TaskType>({
+                id: 'taskId',
+                status: TaskStatuses.InProgress,
+                todoListId: 'todolistId',
+                startDate: '1',
+                priority: TaskPriorities.Urgently,
+                order: 1,
+                deadline: '1',
+                title: title,
+                description: 'yo yo yo',
+                addedDate: "2"
+            })
+
+            if (!title) {
+                setTitle('null')
+            }
+
+            return (
+                <Task
+                    task={task}
+                    todolistId={'todolistId'}
+                    changeTaskStatus={() => {}}
+                    changeTaskTitle={setTitle}
+                    removeTask={() => {}}
+                />
+            );
+        }
+    ]
+} as ComponentMeta<typeof Task>;
+
+const changeTaskStatus = async (...params: any) => {
+    action('Status Change')(...params);
+}
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args}/>;
+export const View = Template.bind({});
+
+
+
+
+
