caa3921c17348c5c99c4ffdf46bddd39d887439c
KAST13L <o4enbsmeshno@gmail.com>
2023-01-08 11:28:39 +0200

    fix: fix AddItemForm.stories.tsx

--- src/components/AddItemForm/AddItemForm.stories.tsx
+++ src/components/AddItemForm/AddItemForm.stories.tsx
@@ -1,25 +1,19 @@
 import React from 'react'
 import {action} from '@storybook/addon-actions'
 import {AddItemForm} from './AddItemForm'
+import {ComponentMeta, ComponentStory} from "@storybook/react";
 
 export default {
     title: 'Universal Components/AddItemForm Stories',
     component: AddItemForm
-}
+} as ComponentMeta<typeof AddItemForm>;
 
-const asyncCallback = async (...params: any[]) => {
+const asyncCallback = async (...params: any) => {
     action('Button inside form clicked')(...params);
 }
-
-export const AddItemFormBaseExample = (props: any) => {
-    return (<AddItemForm
-        addItem={asyncCallback}
-    />)
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args}/>;
+export const View = Template.bind({});
+View.args = {
+    addItem: asyncCallback
 }
 
-
-export const AddItemFormDisabledExample = (props: any) => {
-    return (<AddItemForm disabled={true}
-        addItem={asyncCallback}
-    />)
-}
