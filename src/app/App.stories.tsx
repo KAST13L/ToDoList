bd4dd2fba25a5f1073f1a65f756ec3c2ac0ab025
KAST13L <o4enbsmeshno@gmail.com>
2023-01-11 19:51:16 +0200

    fix: change routes is app.component

--- src/app/App.stories.tsx
+++ src/app/App.stories.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 import {App} from './App';
 import {Provider} from "react-redux";
 import {store} from "@app/app/store";
+import {BrowserRouter} from "react-router-dom";
 
 export default {
     title: 'App/App',
     component: App,
 } as ComponentMeta<typeof App>;
 
-const Template: ComponentStory<typeof App> = (args) => <Provider
+const Template: ComponentStory<typeof App> = (args) => <BrowserRouter><Provider
     store={store}>
     <App demo={true} {...args} />
-</Provider>;
+</Provider></BrowserRouter>;
 
 export const View = Template.bind({});
+View.args = {
+    demo: false
+}
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -36,7 +36,7 @@ export const App: FC<AppPropsType> = ({demo = false}) => {
             <Header/>
             <div className='mx-8'>
                 <Routes>
-                    <Route path='/todolist' element={<TodolistsList/>}/>
+                    <Route path='/todolist' element={<TodolistsList demo={demo}/>}/>
                     <Route path='/login' element={<Login/>}/>
                     <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
                     <Route path='*' element={<Navigate to={'/404'}/>}/>
