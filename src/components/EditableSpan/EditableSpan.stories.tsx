4fee692d6c02663dba391f3706a5d1ab0bacf5ad
KAST13L <o4enbsmeshno@gmail.com>
2023-01-16 13:29:10 +0200

    fix: change action call

--- src/components/EditableSpan/EditableSpan.stories.tsx
+++ src/components/EditableSpan/EditableSpan.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {EditableSpan} from './EditableSpan'
 import {ComponentMeta, ComponentStory} from "@storybook/react";
 import {action} from "@storybook/addon-actions";
