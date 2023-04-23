71f0a2fdfb56d0aa50ba074a9cd5cfa3f50dee28
KAST13L <o4enbsmeshno@gmail.com>
2023-04-23 22:46:47 +0300

    fix: fixing the ability to change the name of the task or the task list until the current request is completed

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -3,11 +3,7 @@ import {ResponseType} from '../api/todolists-api'
 import {Dispatch} from "redux";
 
 export function handleServerAppError<D>(data: ResponseType<D>, dispatch: Dispatch) {
-    if (data.messages.length) {
-        dispatch(setAppError({error: data.messages[0]}))
-    } else {
-        dispatch(setAppError({error: 'Some error occurred'}))
-    }
+    dispatch(setAppError({error: data.messages.length ? data.messages[0] : 'Some error occurred'}))
     dispatch(setAppStatus({status: 'failed'}))
 }
 
@@ -15,17 +11,3 @@ export function handleServerNetworkError(error: { message: string }, dispatch: D
     dispatch(setAppError({error: error.message ? error.message : 'Some error occurred'}))
     dispatch(setAppStatus({status: 'failed'}))
 }
-
-/*
-export function* handleServerNetworkError(error: { message: string }) {
-    yield put(setAppErrorAC({error: error.message ? error.message : 'Some error occurred'}))
-    yield put(setAppStatusAC({status: 'failed'}))
-}
-export function* handleServerAppError<D>(data: ResponseType<D>) {
-    if (data.messages.length) {
-        yield put(setAppErrorAC({error: data.messages[0]}))
-    } else {
-        yield put(setAppErrorAC({error: 'Some error occurred'}))
-    }
-    yield put(setAppStatusAC({status: 'failed'}))
-}*/
