be217a7218a052c468f455e81f3562e940f77484
KAST13L <o4enbsmeshno@gmail.com>
2023-04-26 21:24:10 +0300

    fix: deleted demo in components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React, {FC, useEffect} from 'react'
 import {useSelector} from 'react-redux'
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
-import {Navigate, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {CircularProgress} from "@mui/material";
 import {appActions} from "./app.reducer";
 import {Login} from "@app/features/Auth/login.component";
@@ -10,11 +10,7 @@ import {Header} from "@app/features/Header/header.component";
 import {selectIsInitialized} from "@app/app/selectors";
 import {useActions} from "@app/app/store";
 
-type AppPropsType = {
-    demo?: boolean
-}
-
-export const App: FC<AppPropsType> = ({demo = false}) => {
+export const App: FC = () => {
 
     const isInitialized = useSelector(selectIsInitialized)
     const {initializeApp} = useActions(appActions)
@@ -23,21 +19,18 @@ export const App: FC<AppPropsType> = ({demo = false}) => {
         initializeApp()
     }, [])
 
-    if (!demo && !isInitialized) {
+    if (!isInitialized) {
         return <div className="fixed w-[100%] text-center top-[30%]"><CircularProgress/>
         </div>
     }
     return (
         <span>
             <ErrorSnackbar/>
-            <Header demo={demo}/>
+            <Header/>
             <div className='mx-8'>
                 <Routes>
-                    <Route path='/todolist' element={<TodolistsList/>}/>
                     <Route path='/' element={<TodolistsList/>}/>
                     <Route path='/login' element={<Login/>}/>
-                    <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
-                    <Route path='*' element={<Navigate to={'/404'}/>}/>
                 </Routes>
             </div>
         </span>
--- src/features/Header/header.component.tsx
+++ src/features/Header/header.component.tsx
@@ -6,15 +6,11 @@ import Button from "@mui/material/Button";
 import {AddItemForm} from "@app/components/AddItemForm/AddItemForm";
 import {todolistActions} from "@app/features/Todolist/todolists.reducer";
 import {AppBar} from "@mui/material";
-import {action} from "@storybook/addon-actions";
 import {selectIsLoggedIn, selectStatus} from "@app/app/selectors";
 import {useActions} from "@app/app/store";
 
-interface HeaderPropsType {
-    demo?: boolean
-}
 
-export const Header: FC<HeaderPropsType> = ({demo = false}) => {
+export const Header: FC = () => {
 
     const status = useSelector(selectStatus)
     const isLoggedIn = useSelector(selectIsLoggedIn)
@@ -22,22 +18,19 @@ export const Header: FC<HeaderPropsType> = ({demo = false}) => {
     const {logout} = useActions(authActions)
     const {addTodolist} = useActions(todolistActions)
 
-    const isAuthorizedAndIsDemo = demo || isLoggedIn
-
     return (
         <AppBar position={'static'} color='default'>
             <div
                 className='flex justify-between items-center p-3 text-center smw:flex-col'>
-                {!isAuthorizedAndIsDemo && <div></div>}
                 <span className='flex justify-between smd:flex-col smw:mb-2'>
                     <span className='text-5xl smd:mb-3'>TODOLIST</span>
-                    <span className='px-3'>{isAuthorizedAndIsDemo &&
+                    <span className='px-3'>{isLoggedIn &&
                         <AddItemForm
-                            addItem={demo ? action('add todolist') : addTodolist}/>}</span>
+                            addItem={addTodolist}/>}</span>
                 </span>
                 <span>
-                    {isAuthorizedAndIsDemo &&
-                        <Button onClick={demo ? action('logout') : logout}
+                    {isLoggedIn &&
+                        <Button onClick={logout}
                                 color={'error'}
                                 variant={'contained'}>Log out</Button>}
                 </span>
